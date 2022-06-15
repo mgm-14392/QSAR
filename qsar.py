e012d853b5a28f3e206b028fb97d73badaeb3692
gonzalez <mgm_14392@hotmail.com>
2022-06-15 16:10:27 +0200

    qsar svm

--- qsar.py
+++ qsar.py
@@ -2,29 +2,34 @@ import pandas as pd
 from sklearn.model_selection import GridSearchCV
 from sklearn.svm import SVR
 import numpy as np
+from yaml import load, Loader, dump
+import os
 
+cwd = os.getcwd()
 
-path = '/Users/marianagonzmed/Desktop/ThesisStuff/shapeNW_training/descriptors/test/test_descriptors_clean_activity_data_test_P03367.csv'
-descriptors = pd.read_csv(path)
-zero_cols = [ col for col, is_zero in ((descriptors == 0).sum() == descriptors.shape[0]).items() if is_zero ]
-print(len(zero_cols))
+def write_config(filename: str, args: dict):
+    """ Write a dictionary to a .yml file"""
+    with open(filename, 'w') as file:
+        documents = dump(args, file)
 
-def supportvector(train_data, train_labels, cv=5, n_jobs=-1, find_hparams=True):
+def supportvector(train_data, train_labels, cv=5, n_jobs=-1, find_hparams=True, working_dir=cwd, config_file=None):
 
     params = {
-        'kernel':['linear', 'poly', 'rbf', 'sigmoid'],
-        'C':np.arange(1,42,10),
-        'degree':np.arange(3,6),
-        'coef0':np.arange(0.001,3,0.5),
-        'gamma': ('auto', 'scale'),
-        'epsilon':np.arange(0.1,0.6,0.1)
+        'C':[1, 10, 100, 1000, 10000],
+        'gamma': [1e-6, 1e-5, 1e-4, 1e-3, 1e-2, 1e-1]
     }
 
     if find_hparams:
         model = GridSearchCV(SVR(), param_grid=params, n_jobs=n_jobs, cv=cv, verbose=1)
-    else:
-        model = SVR(**config)
+    #else:
+    #    model = SVR(**config)
 
     model.fit(train_data, train_labels)
+
+    if config_file is not None:
+        if not os.path.exists(config_file):
+            write_config(config_file, model.best_params_)
+    if config_file is None:
+        write_config(os.path.join(working_dir, 'configures', 'SVM.yml'), model.best_params_)
 
     return model
